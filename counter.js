94073ea04eaefb3da8b22f4dccbaa4588d654392
Robin Roy <robin.roy@nexergroup.com>
2024-11-29 16:07:03 +0000

    Added word cloud processing and downlaod as png

---
+++ counter.js
@@ -0,0 +1,36 @@
+function updateWordCount(value) {
+    document.getElementById('word-count-label').innerText = value;
+}
+
+//Generate pop up WIP
+
+//document.addEventListener('DOMContentLoaded', function () {
+
+    //function setupPopup(buttonId, overlayId, closeId) {
+      //  const popupOverlay = document.getElementById(overlayId);
+      //  const closePopup = document.getElementById(closeId);
+      //  const button = document.getElementById(buttonId);
+
+      //  function openPopup() {
+      //      popupOverlay.style.display = 'block';
+      //  }
+
+      //  function closePopupFunc() {
+      //      popupOverlay.style.display = 'none';
+       // }
+
+      //  button.addEventListener('click', openPopup);
+       // closePopup.addEventListener('click', closePopupFunc);
+       // popupOverlay.addEventListener('click', function (event) {
+      //      if (event.target === popupOverlay) {
+       //         closePopupFunc();
+      //      }
+      //  });
+  //  }
+
+  //  setupPopup('settings-btn', 'settings-popupOverlay', 'settings-closePopup');
+  //  setupPopup('save-btn', 'save-popupOverlay', 'save-closePopup');
+  //  setupPopup('import-btn', 'import-popupOverlay', 'import-closePopup');
+//});
+
+//broken pls fix later );
